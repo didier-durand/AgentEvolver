c286b2fbbe4db44d032e2534d0d8f9c4fa4093e2
cc <cc445845@alibaba-inc.com>
2025-07-07 16:45:15 +0800

    chore: update qwen2.5 3b script

--- examples/run_qwen25-3b_8gpu.sh
+++ examples/run_qwen25-3b_8gpu.sh
@@ -18,17 +18,18 @@ python3 -m beyondagent.main_ppo \
     --config-name='beyond_agent_dataflow' \
     env_service.env_url=$env_url \
     algorithm.adv_estimator=grpo \
-    data.train_batch_size=16 \
+    data.train_batch_size=32 \
     data.max_prompt_length=4096 \
     data.max_response_length=20480 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.return_raw_chat=True \
+    actor_rollout_ref.rollout.enable_request_id=False \
     actor_rollout_ref.rollout.prompt_length=20480 \
-    actor_rollout_ref.rollout.response_length=4096 \
+    actor_rollout_ref.rollout.response_length=2048 \
     actor_rollout_ref.rollout.max_model_len=20480 \
     actor_rollout_ref.rollout.temperature=0.9 \
-    actor_rollout_ref.model.path=/mnt/data_cpfs/xielipeng.xlp/models/Qwen2.5-7B-Instruct \
+    actor_rollout_ref.model.path=/mnt/data_cpfs/xielipeng.xlp/models/Qwen3-8B \
     actor_rollout_ref.rollout.use_qwen3=False \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
@@ -42,7 +43,7 @@ python3 -m beyondagent.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
@@ -50,14 +51,14 @@ python3 -m beyondagent.main_ppo \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
-    trainer.n_gpus_per_node=2 \
+    trainer.n_gpus_per_node=8 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='beyondagent' \
-    trainer.experiment_name='qwen25-7b_appworld_zyp' \
+    trainer.experiment_name='qwen25_3b-taskmanager-debug' \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
-    trainer.test_freq=20 \
+    trainer.test_freq=10 \
     trainer.total_epochs=15 \
     trainer.val_before_train=True \
     trainer.validation_data_dir="experiments/exp_${current_time}/validation_log" \
@@ -69,4 +70,8 @@ python3 -m beyondagent.main_ppo \
     critic.forward_max_token_len_per_gpu=20480 \
     data.train_files=/mnt/data_cpfs/zouanni.zan/data/appworld_parquet/train.parquet \
     data.val_files=/mnt/data_cpfs/zouanni.zan/data/appworld_parquet/dev.parquet \
+    experience_maker.enable_summarizer=False \
+    experience_maker.enable_context_generator=False \
+    experience_maker.workspace_id="w1_qwen25_v2_${current_time}" \
+    2>&1 | tee "$log_file" \
     $@
